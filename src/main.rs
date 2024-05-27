0ca9ca41ffddd49554d9703ba6fa0e53aa679884
manu <you@example.com>
2024-05-27 18:48:36 +0200

    Add REPL loop

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,18 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // eprintln!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    // REPL loop
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    if let Ok(_c) = Command::from_str(&input) {
-    } else {
-        println!("{}: command not found", &input.trim())
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        if let Ok(_c) = Command::from_str(&input) {
+        } else {
+            println!("{}: command not found", &input.trim())
+        }
     }
 }
