f28bf7d028645c8d00a0b5739a0f9c2ac438608b
manu <you@example.com>
2024-05-27 18:44:25 +0200

    Add command parsing boiler plate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Command {}
+
+#[derive(Debug, PartialEq, Eq)]
+struct CommandParsingError;
+
+impl FromStr for Command {
+    type Err = CommandParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            _ => Err(CommandParsingError),
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,4 +30,8 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    if let Ok(_c) = Command::from_str(&input) {
+    } else {
+        println!("{}: command not found", &input)
+    }
 }
